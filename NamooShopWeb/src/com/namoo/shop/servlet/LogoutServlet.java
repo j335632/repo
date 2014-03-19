cced3d47cb6400ea2d4741bc7cb3101f7de7c83b
kosta-16 <kosta-16@kosta-16-PC>
2014-03-19 11:52:27 +0900

    add

--- NamooShopWeb/src/com/namoo/shop/servlet/LogoutServlet.java
+++ NamooShopWeb/src/com/namoo/shop/servlet/LogoutServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/logout")
-public class LogoutServlet extends HttpServlet {
+ public class LogoutServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -2226778834596204952L;
 
